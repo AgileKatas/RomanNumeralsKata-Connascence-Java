00ed29d2ed1516f17beead10795ba54b8b98b2e2
Agile Katas <contact@agilekatas.co.uk>
2016-11-02 15:53:55 +0000

    Fix broken test

--- src/main/java/uk/co/agilekatas/romannumerals/RomanNumeralsConverter.java
+++ src/main/java/uk/co/agilekatas/romannumerals/RomanNumeralsConverter.java
@@ -1,13 +1,14 @@
 package uk.co.agilekatas.romannumerals;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 public class RomanNumeralsConverter {
 
   private final Map<Integer, String> values;
 
   public RomanNumeralsConverter(Map<Integer, String> values) {
-    this.values = values;
+    this.values = new TreeMap<>(values).descendingMap();
   }
 
   public String toNumeral(int number) {
